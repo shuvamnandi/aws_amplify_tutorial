10d7d7df3ba33ed429d6c452e6224db75b39dffe
Shuvam Nandi <shuvamnandi9@gmail.com>
2022-02-28 23:17:02 +0800

    Updated App.js and fixed error

--- src/App.js
+++ src/App.js
@@ -7,10 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to This React App!
         </p>
-        Welcome to This React App!
-        </a>
       </header>
     </div>
   );
